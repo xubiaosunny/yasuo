962b5f2fa0afb23575b256ca59d0c7971a330305
xubiao <xubiaosunny@163.com>
2019-05-10 17:45:43 +0800

    fix

--- api/views/user.py
+++ api/views/user.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import get_object_or_404
 
 from api.serializer.user import UserInfoSerializer, UserFollowSerializer
 from utils.common.response import *
@@ -12,9 +13,10 @@ class UserCityView(generics.GenericAPIView):
 
     def get(self, request):
         """
-        获取城市列表, 如果传入`province`参数则返回对应省份的城市列表，不传则返回所有省份对应的城市列表
+        获取城市列表, 如果传入`province` get参数则返回对应省份的城市列表，不传则返回所有省份对应的城市列表
+        如：/api/user/cities/?province=广东省
         """
-        province = request.data.get('province', None)
+        province = request.GET.get('province', None)
         if province is None:
             data = [{"name": k, "city": v} for k, v in CITY.items()]
             data.sort(key=lambda x: x['name'])
@@ -32,9 +34,16 @@ class UserInfoView(generics.GenericAPIView):
 
     def get(self, request):
         """
-        获取用户信息
+        获取用户信息, 默认为本人的用户信息，传入get参数id则获取对应id的用户信息
+        如：/api/user/info/?id=1
         """
-        return response_200(request.user.to_dict())
+        user_id = request.GET.get('id', None)
+        if user_id:
+            user = get_object_or_404(CustomUser, pk=user_id)
+        else:
+            user = request.user
+
+        return response_200(user.to_dict())
 
     def patch(self, request):
         """更新用户信息"""
@@ -55,11 +64,18 @@ class UserFollowView(generics.GenericAPIView):
 
     def get(self, request):
         """
-        获取关注和被关注的用户
+        获取关注和被关注的用户，默认返回为本人的用户关注信息，传入get参数id则获取对应id的用户关注信息
+        如：/api/user/follow/?id=1
         """
+        user_id = request.GET.get('id', None)
+        if user_id:
+            user = get_object_or_404(CustomUser, pk=user_id)
+        else:
+            user = request.user
+
         data = {
-            'my_follow': [u.to_dict() for u in request.user.follow.all()],
-            'follow_me': [u.to_dict() for u in request.user.customuser_set.all()]
+            'my_follow': [u.to_dict() for u in user.follow.all()],
+            'follow_me': [u.to_dict() for u in user.customuser_set.all()]
         }
         return response_200(data)
 
