da2b483650560e2e88d03da6ae626ec96aab439c
xubiao <xubiaosunny@163.com>
2019-05-23 18:21:10 +0800

    model

--- db/db_models/works.py
+++ db/db_models/works.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 from django.conf import settings
 
 
-__all__ = ['Works', 'WorksComment']
+__all__ = ['Works', 'WorksComment', 'WorksAsk']
 
 
 class Works(models.Model):
@@ -12,6 +12,7 @@ class Works(models.Model):
     from .storage import LocalStorage
 
     user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    type = models.CharField(_('Type'), max_length=50, null=True, blank=True)
     storage = models.ForeignKey(LocalStorage, on_delete=models.PROTECT)
     summary = models.TextField(_('Summary'), default='', blank=True)
     location = models.CharField(_('Location'), max_length=50, null=True, blank=True)
@@ -30,8 +31,14 @@ class WorksComment(models.Model):
     update_time = models.DateTimeField(_('Update Time'), auto_now=True)
 
 
-class WorksAsk(models.Model):
+class WorksQuestion(models.Model):
     from .auth import CustomUser
     to = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
     question = models.TextField(_('Question'))
+    create_time = models.DateTimeField(_('Create Time'), auto_now_add=True)
+
+
+class WorksQuestionReply(models.Model):
+    works_question = models.ForeignKey(WorksQuestion, on_delete=models.PROTECT)
+    is_pay = models.BooleanField(default=False)
     create_time = models.DateTimeField(_('Create Time'), auto_now_add=True)
