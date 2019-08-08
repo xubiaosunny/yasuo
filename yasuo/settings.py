d68d3eeea0d4656f077639d1ebb85131db065e78
xubiao <xubiaosunny@163.com>
2019-08-08 12:00:28 +0800

    setting

--- yasuo/settings.py
+++ yasuo/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+    # 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 MIDDLEWARE = [
